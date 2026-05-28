68c822b8d078cb5f5f341f0abe9c4aa91a406f44
Santiago Benavent <A01646121@tec.mx>
2026-05-27 18:34:18 -0600

    singlecycle

---
+++ verilog/single_cycle/ALU_decoder.v
@@ -0,0 +1,48 @@
+module ALU_decoder(
+    input  [1:0]  ALUOp,
+    input  [2:0]  funct3,
+    input         op5,
+    input         funct7_5,
+    output [31:0] result,
+    output        Zero
+);
+reg  [31:0] A,
+reg [31:0] B,
+reg [2:0] Alucontrol;
+
+always @(*) begin
+    case (ALUOp)
+        2'b00: Alucontrol = 3'b000; 
+        2'b01: Alucontrol = 3'b001; 
+
+        2'b10: begin
+            case (funct3)
+                3'b000: begin
+                    if (op5 && funct7_5)
+                        Alucontrol = 3'b001; 
+                    else
+                        Alucontrol = 3'b000; 
+                end
+
+                3'b010: Alucontrol = 3'b101; 
+                3'b110: Alucontrol = 3'b011; 
+                3'b111: Alucontrol = 3'b010; 
+
+                default: Alucontrol = 3'b000;
+            endcase
+        end
+
+        default: Alucontrol = 3'b000;
+    endcase
+end
+
+ALU DECODER (
+    .A(A),
+    .B(B),
+    .Alucontrol(Alucontrol),
+    .result(result),
+    .Zero(Zero)
+);
+
+endmodule
+
---
+++ verilog/single_cycle/Register_file.v
@@ -0,0 +1,28 @@
+module Register_file(
+    input clk,WE3
+    input[4:0] A1,A2,A3
+    input [31:0] WD3,
+    output [31:0] RD1,RD2
+);
+
+reg [31:0] REG [31:0];
+
+always @(posedge clk)
+begin
+    RD1= REG[A1];
+    RD2= REG[A2];
+end
+begin
+    if (WE3 ==1)
+        REG[A3] = WD3;
+end
+
+endmodule
+
+
+
+
+
+
+
+endmodule
